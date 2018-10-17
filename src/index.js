5864f227ceba3c1b3761e90fca1c1bf9297aae2b
Toptal <rlatprnr@gmail.com>
2018-10-17 12:51:01 -0700

    redux init.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
 import './index.css';
-import App from './App';
+import { store } from './redux/store';
+import Routes from './routes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  (
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes />
+      </BrowserRouter>
+    </Provider>
+  ),
+  document.getElementById('root')
+);
 registerServiceWorker();
