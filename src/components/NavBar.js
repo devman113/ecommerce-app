d684196a09e6109e1d076fde221a4a7f7a6ece76
Toptal <rlatprnr@gmail.com>
2018-10-17 18:31:33 -0700

    fix Typo bug.

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,6 @@ import { withStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
@@ -157,9 +156,9 @@ class PersistentDrawer extends React.Component {
               >
                 <MenuIcon />
               </IconButton>
-              <Typography variant="h6" color="inherit" noWrap>
+              <h3>
                 Source Squad
-              </Typography>
+              </h3>
               <SearchBox />
             </Toolbar>
           </AppBar>
